0c85fa7ff94cd6dfae41cadfbcfd4e7500c26a12
aifly <aifly_xc@163.com>
2016-09-12 20:32:48 +0800

    test draw line

--- components/draw-scene.jsx
+++ components/draw-scene.jsx
@@ -8,7 +8,7 @@ export default class DrawScene extends Component {
 		return (
 			<div className='fly-draw-scene'>
 				<header>开始绘制我的私人订制月亮吧</header>
-				<div className='fly-draw-C'>
+				<div className='fly-draw-C' ref='fly-draw-C'>
 					<canvas ref='draw'></canvas>
 					<div className='fly-center-circle'></div>
 				</div>
@@ -21,22 +21,56 @@ export default class DrawScene extends Component {
 	}
 	componentDidMount() {
 		 
-		this.canvasInit(this.setSize());
+		setTimeout(()=>{
+			this.canvasInit(this.setSize());
+		},1)
 	}
 
 	canvasInit(size){
-		let canvas = this.refs['draw'];
+		let canvas = this.refs['draw'],
+			offsetTop = this.refs['fly-draw-C'].offsetTop;
+			 
 		var stage = new createjs.Stage(canvas);
 
 		let shape = new createjs.Shape();
 
-		shape.graphics.beginFill('').drawRect(0,0,size,size);
-		shape.on('mousedown',()=>{
-			alert(23)
-		});
+
+		this.shape = shape;
+
+		let touchmoveHandler = e=>{
+			var x = e.changedTouches[0].pageX,
+				y = e.changedTouches[0].pageY-offsetTop;
+			this.draw(x,y,false);
+			stage.update();
+		}
 
 		stage.addChild(shape);
-		stage.update();
+
+		let touchendHandler = e=>{
+			document.removeEventListener('touchmove',touchmoveHandler);
+			document.removeEventListener('touchend',touchendHandler);
+		}		
+
+		console.log(offsetTop)
+		
+		canvas.addEventListener('touchstart',e=>{
+			var x = e.changedTouches[0].pageX,
+				y = e.changedTouches[0].pageY-offsetTop;
+			this.draw(x,y);
+			
+			document.addEventListener('touchmove',touchmoveHandler);
+			document.addEventListener('touchend',touchendHandler);
+
+		});
+
+	}
+
+	draw(x,y,flag = true){
+		this.target = this.target || this.shape.graphics.beginStroke('#fff');
+		if(flag){
+			//var target = this.shape.graphics.beginStroke('#fff');
+		}
+		this.target[flag ? "moveTo":'lineTo'](x,y);
 	}
 
 	setSize(){
